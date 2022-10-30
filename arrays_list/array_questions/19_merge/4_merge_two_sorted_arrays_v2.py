4d24fa2fa09c6633f7d63ddccf25f5db691485df
Suraj Nathani <surajn222@gmail.com>
2022-10-30 23:38:00 +0530

    Updated merge arrays module

--- arrays_list/array_questions/19_merge/4_merge_two_sorted_arrays_v2.py
+++ arrays_list/array_questions/19_merge/4_merge_two_sorted_arrays_v2.py
@@ -1,24 +1,6 @@
 # Python program to merge two sorted arrays/
 def mergeArrays(arr1, arr2):
-	n1 = len(arr1)
-	n2 = len(arr2)
-	arr3 = [0 for i in range(n1 + n2)]
-	i = 0
-	j = 0
-	k = 0
-
-	# traverse the arr1 and insert its element in arr3
-	while (i < n1):
-		arr3[k] = arr1[i]
-		k += 1
-		i += 1
-
-	# now traverse arr2 and insert in arr3
-	while (j < n2):
-		arr3[k] = arr2[j]
-		k += 1
-		j += 1
-
+	arr3 = arr1 + arr2
 	# sort the whole array arr3
 	arr3.sort()
 	arr3 = map(str, arr3)
